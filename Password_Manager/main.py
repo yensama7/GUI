a70ab589d58d6dc1055328db0a72d03cd2df3e7b
yensama7 <uzonuemerie@gmail.com>
2024-06-24 12:27:04 +0100

    added show password

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -11,7 +11,6 @@ from PIL import Image
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -25,7 +24,7 @@ def generate_password():
     shuffle(password_list)
 
     password = "".join(password_list)
-    password_entry.delete(0,END)
+    password_entry.delete(0, END)
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
@@ -107,7 +106,6 @@ def find_password():
         if website_hashed in data:
             password_key = data[website_hashed]["key"] #Gets key
 
-
             conn = sqlite3.connect('main.db')
             c = conn.cursor()
             c.execute("SELECT * FROM passwords WHERE website=?", (website_hashed,))
@@ -120,65 +118,69 @@ def find_password():
         else:
             messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
+# ---------------------------- TOGGLE PASSWORD VISIBILITY ------------------------------- #
+def toggle_password_visibility(entry, show_password):
+    if show_password.get():
+        entry.configure(show="")
+    else:
+        entry.configure(show="*")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = customtkinter.CTk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
-window.geometry("750x600")
-window.maxsize(750,650)
+window.geometry("700x700")
+window.maxsize(700,700)
 customtkinter.set_appearance_mode("system")
-my_tab = customtkinter.CTkTabview(window,width=700,height=500,corner_radius=20)
+my_tab = customtkinter.CTkTabview(window,width=700,height=600,corner_radius=20)
 my_tab.pack(pady=10)
 
 tab1 = my_tab.add("Add")
 tab2 = my_tab.add("Search")
 
-
 my_image = customtkinter.CTkImage(light_image=Image.open("Password_Manager/logo.png"),dark_image=Image.open("Password_Manager/logo.png"),size=(280, 280))
-img_tab1 = customtkinter.CTkLabel(master=tab1, image= my_image, text="")
+img_tab1 = customtkinter.CTkLabel(master=tab1, image=my_image, text="")
 img_tab1.pack()
 
-#tab 2
-img_tab2 = customtkinter.CTkLabel(master=tab2, image= my_image, text="")
+img_tab2 = customtkinter.CTkLabel(master=tab2, image=my_image, text="")
 img_tab2.pack()
 
-
-#Labels
 website_label_tab1 = customtkinter.CTkLabel(master=tab1,text="Website:")
-website_label_tab1.place(relx=0.2,rely=0.65, anchor=CENTER)
+website_label_tab1.place(relx=0.2,rely=0.55, anchor=CENTER)
 
 website_label_tab2 = customtkinter.CTkLabel(master=tab2,text="Website:")
 website_label_tab2.place(relx=0.2,rely=0.65, anchor=CENTER)
 
 email_label = customtkinter.CTkLabel(master=tab1,text="Email/Username:")
-email_label.place(relx=0.2,rely=0.75, anchor=CENTER)
+email_label.place(relx=0.15,rely=0.65, anchor=CENTER)
 
 password_label = customtkinter.CTkLabel(master=tab1,text="Password:")
-password_label.place(relx=0.2,rely=0.85, anchor=CENTER)
+password_label.place(relx=0.2,rely=0.75, anchor=CENTER)
 
-#Entries
 website_entry_tab1 = customtkinter.CTkEntry(master=tab1,width=250,corner_radius=20)
-website_entry_tab1.place(relx=0.5,rely=0.65,anchor=CENTER)
+website_entry_tab1.place(relx=0.5,rely=0.55,anchor=CENTER)
 website_entry_tab1.focus()
 
 website_entry_tab2 = customtkinter.CTkEntry(master=tab2,width=300,corner_radius=20)
 website_entry_tab2.place(relx=0.5,rely=0.65,anchor=CENTER)
 website_entry_tab2.focus()
 
-
 email_entry = customtkinter.CTkEntry(master=tab1,width=390, corner_radius=20)
-email_entry.place(relx=0.61,rely=0.75, anchor = CENTER)
+email_entry.place(relx=0.62,rely=0.65, anchor = CENTER)
 
-password_entry = customtkinter.CTkEntry(master=tab1,width=250, corner_radius=20)
-password_entry.place(relx=0.5,rely=0.85, anchor=CENTER)
+password_entry = customtkinter.CTkEntry(master=tab1,width=250, corner_radius=20, show="*")
+password_entry.place(relx=0.5,rely=0.75, anchor=CENTER)
 
-# Buttons
+show_password_var = IntVar(value=0)
+show_password_checkbutton = customtkinter.CTkCheckBox(master=tab1,text="show password",variable=show_password_var, command=lambda: toggle_password_visibility(password_entry, show_password_var))
+show_password_checkbutton.place(relx=0.75, rely=0.75, anchor=W)
+
 search_button = customtkinter.CTkButton(master=tab2,text="Search", width=190, command=find_password, corner_radius=20,border_width=1, fg_color="#4158D0",border_color="white")
 search_button.place(relx=0.5,rely=0.75, anchor=CENTER)
 
 generate_password_button = customtkinter.CTkButton(master=tab1,text="Generate Password", command=generate_password,border_width=1,fg_color="#4158D0",corner_radius=20,border_color="white")
-generate_password_button.place(relx=0.85,rely=0.85, anchor=CENTER)
+generate_password_button.place(relx=0.85,rely=0.83, anchor=CENTER)
 
 add_button = customtkinter.CTkButton(master=tab1,text="Add", width=300, command=save,corner_radius=20,fg_color="#4158D0",border_color="white")
 add_button.place(relx=0.55,rely=0.95, anchor=CENTER)
